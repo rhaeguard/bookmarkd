81bf490a9d780adecb6f612f0aa360c48a535b59
MensurOwary <mensurowary@gmail.com>
2020-10-28 01:21:54 +0400

    Greying out the icon when page is already bookmarked feature added

--- scripts/background/background.js
+++ scripts/background/background.js
@@ -1,12 +1,17 @@
 /*global chrome, uuidv4, getMetaInfo*/
 
-function doesNotContain(items, { title, url }) {
-    return !items.some((x) => title === x.title && url === x.url && !x.done);
-}
+const ba = chrome.browserAction;
+const t = chrome.tabs;
 
 function handleDbObj(currentValueString, bookmark) {
     const makeDbObj = (obj) => {
         return { bookmarkieDatabase: JSON.stringify(obj) };
+    };
+
+    const doesNotContain = (items, { title, url }) => {
+        return !items.some(
+            (x) => title === x.title && url === x.url && !x.done
+        );
     };
 
     if (currentValueString) {
@@ -21,9 +26,34 @@ function handleDbObj(currentValueString, bookmark) {
     });
 }
 
-chrome.browserAction.onClicked.addListener(({ title, url }) => {
+function greyOutIcon() {
+    ba.setIcon({
+        path: "icons/gr_16.png",
+    });
+}
+
+function resetIcon() {
+    ba.setIcon({
+        path: "icons/16.png",
+    });
+}
+
+function popAlreadyBookmarked(tabId) {
+    ba.setPopup({
+        tabId,
+        popup: "html/alreadyBookmarked.html",
+    });
+}
+
+function withStore(callback) {
     const store = chrome.storage.sync;
     store.get(["bookmarkieDatabase"], ({ bookmarkieDatabase }) => {
+        callback(store, bookmarkieDatabase);
+    });
+}
+
+ba.onClicked.addListener(({ tabId, title, url }) => {
+    withStore((store, bookmarkieDatabase) => {
         getMetaInfo(url, ({ image, description }) => {
             store.set(
                 handleDbObj(bookmarkieDatabase, {
@@ -33,8 +63,40 @@ chrome.browserAction.onClicked.addListener(({ title, url }) => {
                     image,
                     description,
                     done: false,
-                })
+                }),
+                () => {
+                    greyOutIcon();
+                    popAlreadyBookmarked(tabId);
+                }
             );
         });
+    });
+});
+
+function checkIfAlreadyBookmarked(tabId, url) {
+    withStore((store, bookmarkieDatabase) => {
+        if (bookmarkieDatabase) {
+            const bookmarks = JSON.parse(bookmarkieDatabase).saved;
+            const alreadyBooked = bookmarks
+                .filter((x) => !x.done)
+                .some((x) => url === x.url);
+
+            if (alreadyBooked) {
+                greyOutIcon();
+                popAlreadyBookmarked(tabId);
+            } else {
+                resetIcon();
+            }
+        }
+    });
+}
+
+t.onUpdated.addListener(function (tabId, ci, { url }) {
+    checkIfAlreadyBookmarked(tabId, url);
+});
+
+t.onActivated.addListener(function ({ tabId }) {
+    t.get(tabId, ({ url }) => {
+        checkIfAlreadyBookmarked(tabId, url);
     });
 });
